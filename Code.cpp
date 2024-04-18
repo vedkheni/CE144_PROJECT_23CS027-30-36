c5e83628c35dc1f1010c828de01f2ff43bafaf6a
Tarishijn1208 <149430235+Tarishijn1208@users.noreply.github.com>
2024-04-18 22:28:42 +0530

    Update Code.cpp

--- Code.cpp
+++ Code.cpp
@@ -10,5 +10,26 @@ using namespace std;
 
 int main()
 {
-  return 0;
+ int choice;
+    double radius, length, width, base, height, side, base1, base2, diagonal1, diagonal2;
+    cout << "\n__________________________________\n" ;
+    cout << "|                                |"<< endl;
+    cout << "| -: Area Calculation Program :- |"<< endl;
+    cout << "|                                |"<< endl;
+    cout << "|\t  1. Circle           \t |" << endl;
+    cout << "|\t  2. Rectangle        \t |" << endl;
+    cout << "|\t  3. Triangle         \t |" << endl;
+    cout << "|\t  4. Square           \t |" << endl;
+    cout << "|\t  5. Trapezoid        \t |" << endl;
+    cout << "|\t  6. Parallelogram    \t |" << endl;
+    cout << "|\t  7. Cone             \t |"<< endl;
+    cout << "|\t  8. Cylinder         \t |"<< endl;
+    cout << "|\t  9. Cube             \t |"<< endl;
+    cout << "|\t  10. Pyramid         \t |"<< endl;
+    cout << "|\t  11. Rhombus         \t |"<< endl;
+    cout << "|\t  12. Sphere          \t |"<< endl;
+    cout << "__________________________________"<<endl;
+    cout << " \nEnter your choice : ";
+    cin >> choice;
+    cout << "__________________________________"<<endl<<endl;
 }
