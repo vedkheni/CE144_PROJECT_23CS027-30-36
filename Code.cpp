7f417ba2bf7732f7bc75a5f741203e386da94a77
vedkheni <vedkheni@gmail.com>
2024-04-18 22:04:54 +0530

    main.cpp

--- Code.cpp
+++ Code.cpp
@@ -1,1 +1,14 @@
+/*
+This Project is created by 23CS027_23CS030_23CS036
 
+TOPIC: -: Area Calculation Program :-
+*/
+
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+  return 0;
+}
